8402d78c858ea284bbec7510756f00464a9c43d8
abhijeet <abhijeet40308@gmail.com>
2019-12-19 13:03:49 +0530

    filter functionality

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ class App extends Component {
     super();
 
     this.state = {
-      notes: []
+      notes: [],
+      filterTag: "",
+      filterDate: "",
+      filteredNotes: [],
+      filter: false
     };
   }
 
@@ -40,24 +44,11 @@ class App extends Component {
     });
 
     this.db.ref("notes").on("child_changed", snapshot => {
-      // let note = {
-      //   id: snapshot.key,
-      //   title: snapshot.val().title,
-      //   note: snapshot.val().note,
-      //   tag: snapshot.val().tag,
-      // };
-
       let notes = this.state.notes;
-      
-      // notes = notes.filter(note => note.id !== snapshot.key);
       let index = notes.findIndex(note => note.id === snapshot.key);
-      // console.log(index);
-      // notes.splice(index, 1);
       notes[index].title = snapshot.val().title;
       notes[index].note = snapshot.val().note;
       notes[index].tag = snapshot.val().tag;
-
-      // notes.push(note);
 
       this.setState({
         notes: notes
@@ -74,13 +65,116 @@ class App extends Component {
     });
   }
 
+  onChangeHandler(evt, key) {
+    this.setState({
+      [key]: evt.target.value
+    });
+  }
+
+  filterNoteByTag() {
+    if (this.state.filterTag !== "") {
+      let notes;
+      if (this.state.filteredNotes.length !== 0)
+        notes = this.state.filteredNotes;
+      else notes = this.state.notes;
+      let newNotes = notes.filter(note => note.tag === this.state.filterTag);
+      this.setState({
+        filteredNotes: newNotes,
+        filter: true
+      });
+    }
+  }
+
+  filterNoteByDate() {
+    if (this.state.filterDate !== "") {
+      let date = this.state.filterDate;
+      let yy = date.substr(0, 4);
+      let mm = date.substr(5, 2);
+      let dd = date.substr(8, 2);
+      let newDate = dd + "/" + mm + "/" + yy;
+
+      let notes;
+      if (this.state.filteredNotes.length !== 0)
+        notes = this.state.filteredNotes;
+      else notes = this.state.notes;
+
+      let newNotes = notes.filter(note => note.created_at.includes(newDate));
+      this.setState({
+        filteredNotes: newNotes,
+        filter: true
+      });
+    }
+  }
+
+  clearFilters() {
+    if (
+      this.state.notes.length === 0 ||
+      this.state.filterTag !== "" ||
+      this.state.filterDate !== ""
+    ) {
+      this.setState({
+        filteredNotes: [],
+        filterTag: "",
+        filterDate: "",
+        filter: false
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
         <main>
           <NotesForm />
-          <Notes notes={this.state.notes} />
+          <hr></hr>
+          <section className="noteform-filter">
+            <h3>Search Notes</h3>
+            <div className="form-flex">
+              <span className="form-group">
+                <label htmlFor="noteform-tag">Filter by Tag</label>
+                &nbsp;
+                <input
+                  type="text"
+                  id="noteform-tag"
+                  name="noteform-tag"
+                  value={this.state.filterTag}
+                  onChange={evt => this.onChangeHandler(evt, "filterTag")}
+                />
+                &nbsp;
+                <button onClick={() => this.filterNoteByTag()}>
+                  Filter Notes
+                </button>
+              </span>
+
+              <span className="form-group">
+                <button onClick={() => this.clearFilters()}>
+                  Clear Filters
+                </button>
+              </span>
+
+              <span className="form-group">
+                <label htmlFor="noteform-date">Filter by Date</label>
+                &nbsp;
+                <input
+                  type="date"
+                  id="noteform-date"
+                  name="noteform-date"
+                  value={this.state.filterDate}
+                  onChange={evt => this.onChangeHandler(evt, "filterDate")}
+                />
+                &nbsp;
+                <button onClick={() => this.filterNoteByDate()}>
+                  Filter Notes
+                </button>
+              </span>
+            </div>
+          </section>
+          {this.state.filter ? (
+            <Notes notes={this.state.filteredNotes} />
+          ) : (
+            <Notes notes={this.state.notes} />
+          )}
         </main>
       </div>
     );
